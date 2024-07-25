c0b63e2d11ddd0c7f75d3fd6dfb9289e111ad7ee
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-07-25 12:23:33 +0200

    create showBooks method

--- src/main/java/com/bookvibes/App.java
+++ src/main/java/com/bookvibes/App.java
@@ -1,32 +1,37 @@
 package com.bookvibes;
 import com.bookvibes.classes.Books ;
+import com.bookvibes.structure.dao.BookAllDao;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Books book = new Books();
-        System.out.println("Título: ");
-        book.setTitle(scanner.nextLine());
+//        Scanner scanner = new Scanner(System.in);
+//        Books book = new Books();
+//        System.out.println("Título: ");
+//        book.setTitle(scanner.nextLine());
+//
+//        System.out.println("Autores: ");
+//        book.setAuthor(scanner.nextLine());
+//
+//        System.out.println("Descripción: ");
+//        book.setDescription(scanner.nextLine());
+//
+//        System.out.println("Género: ");
+//        book.setGenre(scanner.nextLine());
+//
+//        System.out.println("ISBN: ");
+//        book.setIsbn(scanner.nextLine());
+//
+//        System.out.println();
+//        System.out.println(book.toString());
+//
+//        saveBookToDatabase(book);
 
-        System.out.println("Autores: ");
-        book.setAuthor(scanner.nextLine());
-
-        System.out.println("Descripción: ");
-        book.setDescription(scanner.nextLine());
-
-        System.out.println("Género: ");
-        book.setGenre(scanner.nextLine());
-
-        System.out.println("ISBN: ");
-        book.setIsbn(scanner.nextLine());
-
-        System.out.println();
-        System.out.println(book.toString());
-
-        saveBookToDatabase(book);
+        BookAllDao booksito = new BookAllDao();
+        System.out.println(booksito.showBooks());
     }
 
     private static void saveBookToDatabase(Books book) {
